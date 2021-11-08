d69dbf346f0d0e91c77b5e785437d981d563daa6
Rajesh01B <80706702+Rajesh01B@users.noreply.github.com>
2021-11-08 21:15:31 +0530

    increment counter

--- script.js
+++ script.js
@@ -1,76 +1,74 @@
-const addButton = document.querySelector('#add');
+const counters = document.querySelector('.counter');
 
-const updateLSData = () => {
-    const textAreaData = document.querySelectorAll('textarea');
-    const notes = [];
-    console.log(textAreaData);
+counters.forEach((counter) => {
+    // console.log(counter);
 
-    textAreaData.forEach((note) => {
-        return notes.push(note.value);
-    })
+    counter.innerHTML = 0;
+    
+    const updateCounter = () => {
+        const targetCount = +counter.getAttribute('data-target')
+        console.log(targetCount);
+    }
+    updateCounter();
 
-    console.log(notes);
-
-    localStorage.setItem('notes',JSON.stringify(notes));
-}
-
-
-const addNewNote = (text = '') => {
-
-    const note = document.createElement('div');
-    note.classList.add('note');
-
-    const htmlData = `
-    <div class="operation">
-        <button class="edit"> <i class="far fa-edit"></i></button>
-        <button class="delete"><i class="fas fa-trash-alt"></i></button>
-    </div>
-
-    <div class="main ${text ? "" : "hidden"}"></div>
-    <textarea class="${text ? "hidden" : "" }"></textarea>`;
-
-    note.insertAdjacentHTML('afterbegin', htmlData );
-    console.log(note);
-
-    // getting the References
-    const editButton = note.querySelector('.edit');
-    const delButton = note.querySelector('.delete');
-    const mainDiv = note.querySelector('.main');
-    const textArea = note.querySelector('textarea');
-
-    // deleting the node 
-    delButton.addEventListener('click', () => {
-        note.remove();
-        updateLSData();
-    })
-
-    // toggle using edit button 
-    textArea.value = text;
-    mainDiv.innerHTML = text;
-
-    editButton.addEventListener('click', () => {
-        mainDiv.classList.toggle('hidden');
-        textArea.classList.toggle('hidden');
-    })
+})
 
 
-    textArea.addEventListener('change',(event) => {
-        const value = event.target.value;
-        mainDiv.innerHTML = text;
-
-        updateLSData();
-    }) 
 
 
-    document.body.appendChild(note);
-    // it appends a node as the last child of a node 
-
-}
 
 
-// getting data back from localStorage
-const notes = JSON.parse(localStorage.getItem('notes'));
 
-if(notes) { notes.forEach((note) =>  localStorage(note) )};
 
-addButton.addEventListener('click', () => addNewNote());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// There are several ways to achieve it:
+
+// using the unary plus operator :
+// var n = +str;
+
+// The Number constructor:
+// var n = number(str);
+
+// The parseFloat function:
+// var n = parseFloat(str)
