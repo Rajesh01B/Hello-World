6592258b9eb57fae9a5f55ce82d7a4dd45a9b144
Rajesh01B <80706702+Rajesh01B@users.noreply.github.com>
2021-11-08 21:14:40 +0530

    google keep clone

--- script.js
+++ script.js
@@ -1,25 +1,76 @@
+const addButton = document.querySelector('#add');
 
-const tempLoad = () => {
-    let temp = document.getElementById('temp');
-    temp.innerHTML = "&#xf2c7";
+const updateLSData = () => {
+    const textAreaData = document.querySelectorAll('textarea');
+    const notes = [];
+    console.log(textAreaData);
 
-    setTime ((property) => {InnerHTML.innerHTML; String
-      temp.innerHTML = "&#xf2ca";
-      temp.style.color = "#f8b627";
-}, 1000);
+    textAreaData.forEach((note) => {
+        return notes.push(note.value);
+    })
 
-setTimeout(() => {
-    temp.innerHTML = "&#xf2c9"
-},2000);
-setTimeout(() => {
-    temp.innerHTML = "&#xf2c8"
-},3000);
-setTimeout(() => {
-    temp.innerHTML = "&#xf2c7"
-},4000);
+    console.log(notes);
+
+    localStorage.setItem('notes',JSON.stringify(notes));
 }
 
-tempLoad();
 
-setInterval(tempLoad,5000)
+const addNewNote = (text = '') => {
 
+    const note = document.createElement('div');
+    note.classList.add('note');
+
+    const htmlData = `
+    <div class="operation">
+        <button class="edit"> <i class="far fa-edit"></i></button>
+        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+    </div>
+
+    <div class="main ${text ? "" : "hidden"}"></div>
+    <textarea class="${text ? "hidden" : "" }"></textarea>`;
+
+    note.insertAdjacentHTML('afterbegin', htmlData );
+    console.log(note);
+
+    // getting the References
+    const editButton = note.querySelector('.edit');
+    const delButton = note.querySelector('.delete');
+    const mainDiv = note.querySelector('.main');
+    const textArea = note.querySelector('textarea');
+
+    // deleting the node 
+    delButton.addEventListener('click', () => {
+        note.remove();
+        updateLSData();
+    })
+
+    // toggle using edit button 
+    textArea.value = text;
+    mainDiv.innerHTML = text;
+
+    editButton.addEventListener('click', () => {
+        mainDiv.classList.toggle('hidden');
+        textArea.classList.toggle('hidden');
+    })
+
+
+    textArea.addEventListener('change',(event) => {
+        const value = event.target.value;
+        mainDiv.innerHTML = text;
+
+        updateLSData();
+    }) 
+
+
+    document.body.appendChild(note);
+    // it appends a node as the last child of a node 
+
+}
+
+
+// getting data back from localStorage
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+if(notes) { notes.forEach((note) =>  localStorage(note) )};
+
+addButton.addEventListener('click', () => addNewNote());
