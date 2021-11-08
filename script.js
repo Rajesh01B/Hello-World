0e5731377fd96d31393a0de13bf452b4da4e79d9
Rajesh01B <80706702+Rajesh01B@users.noreply.github.com>
2021-11-08 21:17:41 +0530

    temperature converter

--- script.js
+++ script.js
@@ -1,74 +1,28 @@
-const counters = document.querySelector('.counter');
+const calculateTemp = () => {
 
-counters.forEach((counter) => {
-    // console.log(counter);
+    const numberTemp = document.getElementById('temp').value;
+    // console.log(numberTemp);
 
-    counter.innerHTML = 0;
-    
-    const updateCounter = () => {
-        const targetCount = +counter.getAttribute('data-target')
-        console.log(targetCount);
+    const tempSelected = document.getElementById('temp_diff');
+    const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
+    // console.log(valueTemp);
+
+    const celToFah = (cel) => {
+        let fahrenheit = Math.round((cel * 9/5 + 32));
+        return fahrenheit;
     }
-    updateCounter();
+    const fahToCel = (fah) => {
+        let fahrenheit = Math.round((fah - 32) * 5/9);
+        return celcius;
+    }
 
-})
+    let result;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// There are several ways to achieve it:
-
-// using the unary plus operator :
-// var n = +str;
-
-// The Number constructor:
-// var n = number(str);
-
-// The parseFloat function:
-// var n = parseFloat(str)
+    if(valueTemp == 'cel'){
+        result = celToFah(numberTemp);
+        document.getElementById('resultContainer').innerHTML= `= ${result}°Fahrenheit`
+    }else{
+        result = fahToCel(numberTemp);
+        document.getElementById('resultContainer').innerHTML= `= ${result}°Celcius`
+    }
+}
